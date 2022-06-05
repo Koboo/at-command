3d18879561c976ac9e72f2376e4cd197b1bb231d
Koboo <14143908+Koboo@users.noreply.github.com>
2022-06-05 15:11:45 +0200

    Remove lombok annotations from CommandSyntax

--- core-at-command/src/main/java/dev/binflux/atcommand/environment/meta/CommandSyntax.java
+++ core-at-command/src/main/java/dev/binflux/atcommand/environment/meta/CommandSyntax.java
@@ -1,10 +1,5 @@
 package dev.binflux.atcommand.environment.meta;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
 public record CommandSyntax(String syntax, String permission) {
 
     @Deprecated
