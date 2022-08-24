00d6267a3f09bdd9cc3907fff9240867791fb65e
Koboo <admin@koboo.eu>
2022-08-25 00:20:41 +0200

    Add quality-of-life player name completion on bukkit and bungeecord

--- bukkit-at-command/src/main/java/dev/binflux/atcommand/environment/BukkitCommandMap.java
+++ bukkit-at-command/src/main/java/dev/binflux/atcommand/environment/BukkitCommandMap.java
@@ -1,10 +1,14 @@
 package dev.binflux.atcommand.environment;
 
+import dev.binflux.atcommand.parser.ParserUtility;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.Locale;
 
 public class BukkitCommandMap extends SimpleCommandMap {
 
@@ -20,6 +24,24 @@ public class BukkitCommandMap extends SimpleCommandMap {
         cmdLine = cmdLine.startsWith("/") ? cmdLine : "/" + cmdLine;
         List<String> completions = environment.handleCompletions(sender, cmdLine, super.tabComplete(sender, cmdLine));
         completions.removeIf(comp -> comp.contains(":") || comp.equalsIgnoreCase(""));
+        if (completions.isEmpty()) {
+            String[] arguments = ParserUtility.getArguments(cmdLine);
+            if (arguments.length > 0) {
+                String lastArgument = arguments[arguments.length - 1].toLowerCase(Locale.ROOT);
+                for (Player player : Bukkit.getOnlinePlayers()) {
+                    if(player.getName().toLowerCase(Locale.ROOT).startsWith(lastArgument) && !completions.contains(player.getName())) {
+                        completions.add(player.getName());
+                    }
+                }
+            } else {
+                completions.addAll(
+                        Bukkit.getOnlinePlayers()
+                                .parallelStream()
+                                .map(Player::getName)
+                                .toList()
+                );
+            }
+        }
         return completions;
     }
 }
