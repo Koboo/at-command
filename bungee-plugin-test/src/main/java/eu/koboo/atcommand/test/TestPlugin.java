6413ef1533ffaa18d3b721452c831c9c99fbfa93
Koboo <admin@koboo.eu>
2022-10-26 12:04:35 +0200

    Create bungeecord test-plugin

---
+++ bungee-plugin-test/src/main/java/eu/koboo/atcommand/test/TestPlugin.java
@@ -0,0 +1,19 @@
+package eu.koboo.atcommand.test;
+
+import eu.koboo.atcommand.AtCommand;
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class TestPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        AtCommand.getEnvironment().registerCommandsIn("eu.koboo.atcommand.test.commands",
+                "eu.koboo.atcommand.test.global");
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
