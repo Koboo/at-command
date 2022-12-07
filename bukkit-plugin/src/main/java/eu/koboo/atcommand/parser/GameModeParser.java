6ef8d91809825ab6563448e5a3f7573feb0e5a0b
Koboo <admin@koboo.eu>
2022-12-07 17:45:20 +0100

    Add completion to GameModeParser

--- bukkit-plugin/src/main/java/eu/koboo/atcommand/parser/GameModeParser.java
+++ bukkit-plugin/src/main/java/eu/koboo/atcommand/parser/GameModeParser.java
@@ -3,9 +3,12 @@ package eu.koboo.atcommand.parser;
 import eu.koboo.atcommand.exceptions.ParameterException;
 import org.bukkit.GameMode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
-//TODO: Completion
 public class GameModeParser extends ParameterParser<GameMode> {
 
     private static final GameMode[] VALUES = GameMode.values();
@@ -20,6 +23,31 @@ public class GameModeParser extends ParameterParser<GameMode> {
         throw new ParameterException(value + " is not valid gameMode.");
     }
 
+    @Override
+    public String friendlyName() {
+        return "GameMode";
+    }
+
+    @Override
+    public List<String> complete(String value) {
+        if(value == null) {
+            return Arrays.stream(VALUES)
+                    .map(GameMode::name)
+                    .collect(Collectors.toList());
+        } else {
+            value = value.toLowerCase(Locale.ROOT);
+            List<String> completions = new ArrayList<>();
+            for (GameMode gameMode : VALUES) {
+                String completion = gameMode.name().toLowerCase(Locale.ROOT);
+                if(completion.startsWith(value) && !completions.contains(completion)) {
+                    completions.add(value);
+                }
+            }
+            return completions;
+        }
+    }
+
+    @SuppressWarnings("deprecation")
     private boolean isGameMode(GameMode gameMode, String value) {
         if(value.equalsIgnoreCase(gameMode.name().toLowerCase(Locale.ROOT))) {
             return true;
