4ca1772db705d9dc62c0e5e41e567135931b0533
Koboo <admin@koboo.eu>
2022-12-07 17:45:35 +0100

    Add completion to WorldParser

--- bukkit-plugin/src/main/java/eu/koboo/atcommand/parser/WorldParser.java
+++ bukkit-plugin/src/main/java/eu/koboo/atcommand/parser/WorldParser.java
@@ -4,7 +4,11 @@ import eu.koboo.atcommand.exceptions.ParameterException;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 
-//TODO: Completion
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
 public class WorldParser extends ParameterParser<World> {
 
     @Override
@@ -15,6 +19,25 @@ public class WorldParser extends ParameterParser<World> {
             }
         }
         throw new ParameterException("The world " + value + " could not be found.");
+    }
+
+    @Override
+    public List<String> complete(String value) {
+        if(value == null) {
+            return Bukkit.getWorlds().stream()
+                    .map(World::getName)
+                    .collect(Collectors.toList());
+        } else {
+            value = value.toLowerCase(Locale.ROOT);
+            List<String> completions = new ArrayList<>();
+            for (World world : Bukkit.getWorlds()) {
+                String completion = world.getName().toLowerCase(Locale.ROOT);
+                if(completion.startsWith(value) && !completions.contains(completion)) {
+                    completions.add(value);
+                }
+            }
+            return completions;
+        }
     }
 
     @Override
